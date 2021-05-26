887039428e6031060ff74c5514e5a27ddbf5db22
poochingil <siddhantgreat24@gmail.com>
2021-05-26 20:32:09 +0530

    2nd commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import MainPage from "./Pages/MainPage/MainPage"
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact render={()=> <MainPage/>} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
